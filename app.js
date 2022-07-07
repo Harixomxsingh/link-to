66323308d1f9cb313dd6613ecb14df80559bf997
Hariomsingh333 <www.hariom03.com@gmail.com>
2022-07-08 00:28:53 +0530

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded", () => {
+  VANTA.BIRDS({
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    backgroundAlpha: 0.0,
+  });
+});
+console.log("hello world");
